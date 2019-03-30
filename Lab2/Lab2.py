82d00a4ff5bb072edb0360f511c784b2c9a8c0d0
Sonya <rocketdog2007@comcast.net>
2019-03-30 14:22:49 -0400

    Updated a lot of things

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -18,8 +18,9 @@ tf.set_random_seed(1618)
 tf.logging.set_verbosity(tf.logging.ERROR)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-CONTENT_IMG_PATH = ""           #TODO: Add this.
-STYLE_IMG_PATH = ""             #TODO: Add this.
+CONTENT_IMG_PATH = "content_img/utahSkiLandscape.jpg" 
+STYLE_IMG_PATH = "style_img/starryNight.jpg"
+FINAL_IMG_FILE = 'output_img/starryNight_UtahSkiLandscape'
 
 
 CONTENT_IMG_H = 500
@@ -35,15 +36,25 @@ TOTAL_WEIGHT = 1.0
 TRANSFER_ROUNDS = 3
 
 
-
 # ============================= < Helper Fuctions > ============================= #
 '''
-TODO: implement this.
+Think I fixed this: implement this.
 This function should take the tensor and re-convert it to an image.
 '''
 def deprocessImage(img):
+    if K.image_data_format() == 'channels_first':
+        img = img.reshape((3, img_nrows, img_ncols))
+        img = img.transpose((1, 2, 0))
+    else:
+        img = img.reshape((img_nrows, img_ncols, 3))
+    # Remove zero-center by mean pixel
+    img[:, :, 0] += 103.939
+    img[:, :, 1] += 116.779
+    img[:, :, 2] += 123.68
+    # 'BGR'->'RGB'
+    img = img[:, :, ::-1]
+    img = np.clip(img, 0, 255).astype('uint8')
     return img
-
 
 def gramMatrix(x):
     features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
@@ -51,23 +62,21 @@ def gramMatrix(x):
     return gram
 
 
-
 # ============================= < Loss Function Builder Functions > ============================= #
-
+# gen --> generated image
 def styleLoss(style, gen):
-    return None   #TODO: implement.
-
+    G = gramMatrix(gen)
+    A = gramMatrix(style)
+    return K.sum(K.square(G - A)) / (4. * 3^2 * (CONTENT_IMG_H * CONTENT_IMG_W)^2)
 
 def contentLoss(content, gen):
-    return K.sum(K.square(gen - content))
-
+    return K.sum(K.square(gramMatrix(gen) - gramMatrix(content)))
 
 def totalLoss(x):
-    return None   #TODO: implement.
-
-
-
-
+    a = K.square(x[:, :, :CONTENT_IMG_W - 1, :CONTENT_IMG_H - 1] - x[:, :, 1:, :CONTENT_IMG_H - 1])
+    b = K.square(x[:, :, :CONTENT_IMG_W - 1, :CONTENT_IMG_H - 1] - x[:, :, :CONTENT_IMG_W - 1, 1:])
+    return K.sum(K.pow(a + b, 1.25))
+    # TODO: Understand this ^^^^^
 
 # ============================= < Pipeline Functions > ============================= #
 
@@ -82,7 +91,6 @@ def getRawData():
     return ((cImg, CONTENT_IMG_H, CONTENT_IMG_W), (sImg, STYLE_IMG_H, STYLE_IMG_W), (tImg, CONTENT_IMG_H, CONTENT_IMG_W))
 
 
-
 def preprocessData(raw):
     img, ih, iw = raw
     img = img_to_array(img)
@@ -94,6 +102,13 @@ def preprocessData(raw):
     img = vgg19.preprocess_input(img)
     return img
 
+def f_minLoss():
+    # TODO: Implement this
+    return 
+
+def f_minGrads():
+    # TODO: Implement this
+    return
 
 '''
 TODO: Allot of stuff needs to be implemented in this function.
@@ -105,43 +120,61 @@ Save the newly generated and deprocessed images.
 '''
 def styleTransfer(cData, sData, tData):
     print("   Building transfer model.")
+    # Process and create the 3 images
     contentTensor = K.variable(cData)
     styleTensor = K.variable(sData)
     genTensor = K.placeholder((1, CONTENT_IMG_H, CONTENT_IMG_W, 3))
+    # Ok here we smash everything together into 1 tensor
     inputTensor = K.concatenate([contentTensor, styleTensor, genTensor], axis=0)
-    model = None   #TODO: implement.
+    # Give it to the model
+    model = vgg19.VGG19(include_top=False, weights='imagenet', input_tensor=inputTensor)
+    # Gett the layers
     outputDict = dict([(layer.name, layer.output) for layer in model.layers])
     print("   VGG19 model loaded.")
+    
+    # All this is loss 
     loss = 0.0
     styleLayerNames = ["block1_conv1", "block2_conv1", "block3_conv1", "block4_conv1", "block5_conv1"]
     contentLayerName = "block5_conv2"
+    # We do content loss here
     print("   Calculating content loss.")
     contentLayer = outputDict[contentLayerName]
     contentOutput = contentLayer[0, :, :, :]
     genOutput = contentLayer[2, :, :, :]
-    loss += None   #TODO: implement.
+    loss += CONTENT_WEIGHT * contentLoss(contentOutput, genOutput) # I dunno if this is right --- OK I think it is
+    # we do style loss here
     print("   Calculating style loss.")
     for layerName in styleLayerNames:
-        loss += None   #TODO: implement.
-    loss += None   #TODO: implement.
-    # TODO: Setup gradients or use K.gradients().
+        layer = outputDict[styleLayerNames]
+        styleOutput = layer[1, :, :, :]
+        genOutput = layer[2, :, :, :]
+        w = STYLE_WEIGHT / len(styleLayerNames)
+        layerLoss = styleLoss(styleOutput, genOutput) #hmmmmmmmmmmmmmmmmm we are thinking, loading, processing
+        loss += w * layerLoss # Fingers crossed
+    # and here we do total loss
+    loss += TOTAL_WEIGHT * totalLoss(genTensor)  # OK WE ARE MAKING PROGRESS
+    
+    # Setup gradients or use K.gradients() -- maybe I'm writing ok code
+    gradients = K.gradients(loss, genTensor)
+
+    outputs = K.function([genTensor], [loss].append(grads))
+
+    # This loop is done
     print("   Beginning transfer.")
+    imgData = tData.copy()
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
-        #TODO: perform gradient descent using fmin_l_bfgs_b.
+        
+        minEst, minVal, infoDict = fmin_l_bfgs_b(f_minLoss, tData.flatten(), fprime=f_minGrads, maxfun=20)
         print("      Loss: %f." % tLoss)
-        img = deprocessImage(x)
-        saveFile = None   #TODO: Implement.
-        #imsave(saveFile, img)   #Uncomment when everything is working right.
+        img = deprocessImage(imgData.copy())
+        saveFile = FINAL_IMG_FILE + '_' + i + '.jpg'
+        imsave(saveFile, img)
         print("      Image saved to \"%s\"." % saveFile)
     print("   Transfer complete.")
 
 
-
-
-
 # ============================= < Main > ============================= #
-
 def main():
     print("Starting style transfer program.")
     raw = getRawData()
@@ -149,9 +182,7 @@ def main():
     sData = preprocessData(raw[1])   # Style image.
     tData = preprocessData(raw[2])   # Transfer image.
     styleTransfer(cData, sData, tData)
-    print("Done. Goodbye.")
-
-
+    print("Done. Goodbye :D ")
 
 if __name__ == "__main__":
     main()
